16e0b61f28eff62a0a28151b7cf8a576560247f2
Tarek Salha <tarek.salha.ext@glasfaserplus.de>
2025-05-26 15:12:27 +0200

    turned pamas back to original;

--- params.py
+++ params.py
@@ -1,8 +1,8 @@
-initial_gold: int = 500
-initial_wood: int = 500
-initial_stone: int = 500
+initial_gold: int = 1000
+initial_wood: int = 1000
+initial_stone: int = 1000
 
-initial_warehouse_capacity: int  = 500
+initial_warehouse_capacity: int  = 1000
 
 initial_rate_gold: float = 4.5
 initial_rate_wood: float = 4.0
@@ -13,7 +13,7 @@ max_number_frigates: int = 1
 max_number_cargo_ships: int = 1
 
 max_level_mines: int = 7                    # it is quite unlikely that you will find effective strategies above this level
-max_level_warehouse: int = 3                # the maximum allowed level of the warehouse
+max_level_warehouse: int = 1                # the maximum allowed level of the warehouse
 max_diff_level_mines: int = 7
 
 cost_estimation_parameter: float = 0.98     # determines how much the algorithm should underestimate needed time to reach a final state, do not set to >1.0
